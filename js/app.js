241186977f8977c2f8a7e1924002b24ebb1447c9
Freyr Fridfinnsson <freyr12@ru.is>
2015-12-08 11:17:57 -0500

    update

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ angular.module('nolta').config(['$routeProvider',
 function ($routeProvider) {
   $routeProvider
     .when('/frontpage', { templateUrl: 'views/frontpage.html', controller: 'SimpleController' })
+    .when('/fyrirlestrar', { templateUrl: 'views/fyrirlestrar.html', controller: 'AboutController' })
+    .when('/personulegThroun', { templateUrl: 'views/personulegThroun.html', controller: 'AboutController' })
+    .when('/lidsheild', { templateUrl: 'views/lidsheild.html', controller: 'AboutController' })
+    .when('/stefnumotun', { templateUrl: 'views/stefnumotun.html', controller: 'AboutController' })
     .when('/about', { templateUrl: 'views/about.html', controller: 'AboutController' })
     .otherwise({
       redirectTo: '/frontpage'
